39931369d287aeba4e2721d5b96bab1b3e3c1c8d
molnex <117267309+molnex@users.noreply.github.com>
2026-03-23 21:11:34 +0200

    Revert "added backend logic final"

--- apps/backend/src/routes/search.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as searchController from '../controllers/searchController.js';
-import { asyncHandler } from '../middleware/errorHandler.js';
-
-const router = Router();
-
-router.get('/', asyncHandler(searchController.search));
-
-export default router;
--- apps/backend/src/utils/AppError.js
+++
@@ -1,8 +0,0 @@
-export class AppError extends Error {
-  constructor(code, message, status = 400) {
-    super(message);
-    this.code = code;
-    this.status = status;
-    this.name = 'AppError';
-  }
-}
